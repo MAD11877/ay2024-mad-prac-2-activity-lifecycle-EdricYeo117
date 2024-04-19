3855087e193a9436f158da7caddcbf5082b5adca
EdricYeo117 <edricyeo.jr@gmail.com>
2024-04-20 00:15:40 +0800

    Challenges for Practical 2 completed

--- app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
@@ -1,6 +1,7 @@
 package sg.edu.np.mad.madpractical2;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,7 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button buttonMessage;
+    private boolean followed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        buttonMessage = findViewById(R.id.btnFollow);
+        buttonMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                followed = !followed;
+                updateButtonMessageText();
+            }
         });
 
         // Intialize a new User object
@@ -34,6 +44,17 @@ public class MainActivity extends AppCompatActivity {
         // Set the TextViews with the User's name, description, deault button message
         tvName.setText(user.name);
         tvDescription.setText(user.description);
-        btnFollow.setText("Follow");
+        btnFollow.setText(getString(R.string.button_follow_text));
+
+        // Logic to update based on boolean
+
+    }
+    private void updateButtonMessageText() {
+            if (followed) {
+                buttonMessage.setText(R.string.button_unfollow_text);
+            } else {
+                buttonMessage.setText(R.string.button_follow_text);
+            }
+
     }
 }
